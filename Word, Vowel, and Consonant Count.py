d71e4203b0381dede0580ff39d00ac9c95d050a1
Mary Gwen Susa <Mary Gwen G. Susa>
2021-11-28 23:49:56 +0800

    Input validation if there is none

--- Word, Vowel, and Consonant Count.py
+++ Word, Vowel, and Consonant Count.py
@@ -5,3 +5,5 @@ print(title)
 print("*" * len(title))
 
 toBeCounted = input("\033[42mInput:\033[00m\033[36m ")
+if toBeCounted == None or toBeCounted == "": # input validation-- to make sure there is an input
+    raise Exception("\033[41mThe input is empty.\033[00m")
