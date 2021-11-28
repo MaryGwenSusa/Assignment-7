89644bdd2a9c1a15aa820d438e6150fde211d77d
Mary Gwen Susa <Mary Gwen G. Susa>
2021-11-28 23:49:06 +0800

    Input with color display output

--- Word, Vowel, and Consonant Count.py
+++ Word, Vowel, and Consonant Count.py
@@ -3,3 +3,5 @@ title = "**ALPHA INPUT COUNTER (Words, Vowels, Consonants)**"
 print("*" * len(title)) # created a header design
 print(title)
 print("*" * len(title))
+
+toBeCounted = input("\033[42mInput:\033[00m\033[36m ")
