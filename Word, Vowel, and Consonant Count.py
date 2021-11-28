ede2d5aff269425478e877b0e8042a50ecb0e1b4
Mary Gwen Susa <Mary Gwen G. Susa>
2021-11-29 00:02:11 +0800

    Print results

--- Word, Vowel, and Consonant Count.py
+++ Word, Vowel, and Consonant Count.py
@@ -13,12 +13,10 @@ wordCount = sum([w.strip(string.punctuation).isalpha() for w in toBeCounted.spli
 """The punctuations/special characters/digits in toBeCounted string input will be stripped and confirmed to be alphabets.
 All stripped w's will then be split into a list in a loop which will be summed up
 """
-
 vowel = consonant = 0 # initializations (for creating variables with changing values)
 toBeCounted = toBeCounted.lower()
 """Convert input to lower case to lessen the if conditions read by the program
 """
-
 for v in toBeCounted: # 97 = lowercase a; 101 = lowercase e; 105 = lowercase i; 111 = lowercase o; 117 = lowercase u; 122 = lowercase z
     if(ord(v) == 97 or ord(v) == 101 or ord(v) == 105
        or ord(v) == 111 or ord(v) == 117):
@@ -27,3 +25,7 @@ for v in toBeCounted: # 97 = lowercase a; 101 = lowercase e; 105 = lowercase i; 
         consonant+=1
 """Utilization of American Standard Code for Information Interchange table to represent certain alphabets and minimize the program
 """
+print("\033[00m\033[45mOutput:\033[00m")
+print("\033[43mWord(s):\033[00m\033[93m", wordCount)
+print("\033[00m\033[44mVowel(s):\033[00m\033[34m",vowel)
+print("\033[00m\033[42mConsonant(s):\033[00m\033[92m",consonant)
