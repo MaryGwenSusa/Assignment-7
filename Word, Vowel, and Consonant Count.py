51fc23e03cd932dc86de823c90d7895d03b9d318
Mary Gwen Susa <Mary Gwen G. Susa>
2021-11-28 23:53:54 +0800

    Import string and took count of words by using sum(), strip(), and split() plus comment and docstring

--- Word, Vowel, and Consonant Count.py
+++ Word, Vowel, and Consonant Count.py
@@ -1,4 +1,4 @@
-
+import string
 title = "**ALPHA INPUT COUNTER (Words, Vowels, Consonants)**"
 print("*" * len(title)) # created a header design
 print(title)
@@ -7,3 +7,9 @@ print("*" * len(title))
 toBeCounted = input("\033[42mInput:\033[00m\033[36m ")
 if toBeCounted == None or toBeCounted == "": # input validation-- to make sure there is an input
     raise Exception("\033[41mThe input is empty.\033[00m")
+
+wordCount = sum([w.strip(string.punctuation).isalpha() for w in toBeCounted.split()])
+# string.punctuation = (!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~)
+"""The punctuations/special characters/digits in toBeCounted string input will be stripped and confirmed to be alphabets.
+All stripped w's will then be split into a list in a loop which will be summed up
+"""
