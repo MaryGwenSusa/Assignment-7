93d574875972778bc9804508645679eb974215f8
Mary Gwen Susa <Mary Gwen G. Susa>
2021-12-01 12:44:53 +0800

    New defined function with docstring intended to be displayed with some modifications

--- Password Validator.py
+++ Password Validator.py
@@ -3,3 +3,13 @@ def header():
     print("*" * len(title)) # created a header design
     print(title)
     print("*" * len(title))
+
+def validPassword(inputPassword):
+    """\033[4mEvaluate if the input is valid as a password.\033[0m
+
+    These are the conditions need to be met:
+    if its greater than \033[3m\033[32m15 characters\033[0m;
+    if it has at least \033[3m\033[34mone uppercase letter\033[0m;
+    if it has at least \033[3m\033[35mone numeral\033[0m; and
+    if it has any of the \033[3m\033[36mspecial characters (!#$%&()*+,-./:;<=>?@[\]^_{|}~)\033[0m.
+    """  
