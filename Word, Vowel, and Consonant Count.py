4fe8b1614c0247c5334d1150a4f96befe24a3439
Mary Gwen Susa <Mary Gwen G. Susa>
2021-12-01 12:42:12 +0800

    Made the lower() function beside the input already

--- Word, Vowel, and Consonant Count.py
+++ Word, Vowel, and Consonant Count.py
@@ -4,19 +4,18 @@ print("*" * len(title)) # created a header design
 print(title)
 print("*" * len(title))
 
-toBeCounted = input("\033[42mInput:\033[00m\033[36m ")
+toBeCounted = input("\033[42mInput:\033[00m\033[36m ").lower()
+"""Convert input to lower case to lessen the if conditions read by the program
+"""
 if toBeCounted == None or toBeCounted == "": # input validation-- to make sure there is an input
     raise Exception("\033[41mThe input is empty.\033[00m")
-
+    
 wordCount = sum([w.strip(string.punctuation).isalpha() for w in toBeCounted.split()])
 # string.punctuation = (!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~)
 """The punctuations/special characters/digits in toBeCounted string input will be stripped and confirmed to be alphabets.
 All stripped w's will then be split into a list in a loop which will be summed up
 """
 vowel = consonant = 0 # initializations (for creating variables with changing values)
-toBeCounted = toBeCounted.lower()
-"""Convert input to lower case to lessen the if conditions read by the program
-"""
 for v in toBeCounted: # 97 = lowercase a; 101 = lowercase e; 105 = lowercase i; 111 = lowercase o; 117 = lowercase u; 122 = lowercase z
     if(ord(v) == 97 or ord(v) == 101 or ord(v) == 105
        or ord(v) == 111 or ord(v) == 117):
